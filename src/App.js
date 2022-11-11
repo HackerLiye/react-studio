5f1c3651fb1afe9d86a946fbb5c12e8266768bcb
Youcheng Liye <hackerliye@gmail.com>
2022-11-11 00:40:47 -0500

    done

--- src/App.js
+++ src/App.js
@@ -11,18 +11,39 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
-
+  const [total, setTotal] = useState(0);
+  const [cart, setCart] = useState([]);
+  function add(item) {
+    setTotal(item.price + total);
+    setCart([...cart, item])
+  }
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <div>
+          <img src={item.image} width={500} />
+          <h4><b>{item.name}</b></h4>
+          <p>{item.price}</p>
+          <p>{item.description}</p>
+          <button className="button" onClick={() => add(item)}><span>Add</span></button>
+        </div>
       ))}
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {
+          <div>
+            <p>Total Price: {total}</p>
+            {cart.map((item, index) => (
+              <div key={index}>
+                <p>{item.name}: ${item.price}</p>
+              </div>
+            ))}
+          </div>
+
+        }
       </div>
     </div>
   );
